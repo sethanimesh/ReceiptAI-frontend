55ac2e61aa521ad926bd2e3b9986abaebd9d96c7
animesh <sethanimesh@hotmail.com>
2024-08-28 21:36:22 +0530

    solving build issues

--- src/Components/AuthContext.jsx
+++ src/Components/AuthContext.jsx
@@ -5,7 +5,6 @@ export const AuthContext = createContext()
 export const useAuth = () => useContext(AuthContext)
 
 export default function AuthProvider({children}){
-    const [number, setNumber] = useState(0)
 
     const [isAuthenticated, setAuthenticated] = useState(false)
 
